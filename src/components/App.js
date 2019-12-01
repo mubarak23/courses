98d9bec3243b2ef53739d4b8d631ff8da1795896
mubarak23 <mubarakaminu340@gmail.com>
2019-12-01 12:41:33 +0100

    ft(courses):set courses component that will list available course[progress]

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFount";
+import CoursePage from "./courses/CoursesPage";
 function App() {
   return (
     <div className="container-fluid">
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
