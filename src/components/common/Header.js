76ee371983989cf895b6615deab72bb57474cc2d
mubarak23 <mubarakaminu340@gmail.com>
2019-12-01 12:20:10 +0100

    ft(structure):set header for the app[progress]

---
+++ src/components/common/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  const activeStyle = { color: "#F1582A" };
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" exact activeStyle={activeStyle}>
+          Home
+        </NavLink>
+        {"|"}
+        <NavLink to="/about" activeStyle={activeStyle}>
+          About
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
-    <div className="Jumbotron">
+    <div className="jumbotron">
       <h2>PluralSight Adminstration Panel</h2>
       <p>React Redux and React Router for Ultra responsive Web App</p>
       <Link to="/about" className="btn btn-primary btn-lg">
