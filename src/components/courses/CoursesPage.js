e086d520574bc41fd09a5ceb4baf974be93a0fca
mubarak23 <mubarakaminu340@gmail.com>
2019-12-25 00:57:34 +0100

    ft(course):add form for course creation[progress]

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 class CoursePage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      course: {
+        title: ""
+      }
+    }
+  }
+  handleOnChange(event) {
+    event.preventDefault();
+    const course = { ...this.state.course, title: event.target.value};
+    this.setState({ course })
+  }
   render() {
     return (
       <div>
         <h2>Courses</h2>
+        <form>
+          <h4>Add Course</h4>
+          <input type="text" onChange={this.handleOnChange} value={this.state.course.title} />
+          <input type="submit" value="Save" />
+        </form>
       </div>
     );
   }
