3d397ef2d1f0fb32d55da45f318a9984dd0698f7
mubarak23 <mubarakaminu340@gmail.com>
2019-12-25 01:25:47 +0100

    ft(course):create actions and reducers in redux[progress]

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
 
 class CoursePage extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      course: {
-        title: ""
-      }
+  state = {
+    course: {
+      title: ""
     }
-  }
-  handleOnChange(event) {
+  };
+  //this.handleOnChange = this.handleOnChange.bind(this);
+
+  handleOnChange = event => {
     event.preventDefault();
-    const course = { ...this.state.course, title: event.target.value};
-    this.setState({ course })
-  }
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
   render() {
     return (
       <div>
         <h2>Courses</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h4>Add Course</h4>
-          <input type="text" onChange={this.handleOnChange} value={this.state.course.title} />
+          <input
+            type="text"
+            onChange={this.handleOnChange}
+            value={this.state.course.title}
+          />
           <input type="submit" value="Save" />
         </form>
       </div>
